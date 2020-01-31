8222225d974fbb9ab3658fc6929cdf87dc05844e
Kitiya <kate.kitiya@gmail.com>
2020-01-31 14:40:21 -0500

    React boilerplate

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => (
+  <div>
+    <h1>Hello World</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. A, quo
+      necessitatibus soluta atque veniam odio rerum eligendi! Quo, sed quod
+      voluptas laborum, animi obcaecati facere adipisci, distinctio expedita ex
+      rem?
+    </p>
+  </div>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
